a7f29f1a32c64f81437fa806598bd950b2d577ba
Austin King <shout@ozten.com>
2012-02-10 22:51:45 -0800

    Adding production mode

--- test/connect-cachify-test.js
+++ test/connect-cachify-test.js
@@ -11,8 +11,13 @@ var resetConfig = function () {
     root: ['.'],
     debug: false
   });
-}
+};
 
+var make_assets = function () { return {
+  "/js/main.min.js": [
+    "/js/lib/jquery.js", "/js/lib/jquery-foomatic.js",
+    "/js/main.js"
+  ]}; };
 
 exports.setup = nodeunit.testCase({
   setUp: function (cb) {
@@ -35,29 +40,28 @@ exports.setup = nodeunit.testCase({
       function (err) {
         fs.writeFile('/tmp/js/main.js', "", "utf8", this);
       },
-      function (err) { 
-        console.log('finished setting up'); cb(); 
+      function (err) {
+        console.log('finished setting up'); cb();
       }
     );
-
   },
   tearDown: function (cb) {
     console.log('tearDown');
     step(
       fs.unlink('/tmp/js/main.js', this),
-      function (err) { 
+      function (err) {
         fs.unlink('/tmp/js/lib/jquery-foomatic.js', this);
       },
-      function (err) { 
+      function (err) {
         fs.unlink('/tmp/js/lib/jquery.js', this);
       },
-      function (err) { 
+      function (err) {
         fs.unlink('/tmp/js/main.min.js', this);
       },
-      function (err) { 
+      function (err) {
         fs.rmdir('/tmp/js/lib', this);
       },
-      function (err) { 
+      function (err) {
         fs.rmdir('/tmp/js', this);
       },
       function (err) { cb(); }
@@ -70,28 +74,20 @@ exports.setup = nodeunit.testCase({
     test.done();
   },
   "Development mode with debug": function (test) {
-    var assets = {
-      "/js/main.min.js": [
-        "/js/lib/jquery.js", "/js/lib/jquery-foomatic.js",
-        "/js/main.js"
-      ]
-    };
-    cachify.setup(assets, {
-                      root: ['/tmp'],
-                      production: false, 
-                      debug: true});
-    var links = cachify.cachify_js("/js/main.min.js").split('\n');
+    var assets = make_assets(),
+        mddlwr,
+        links;
+    mddlwr = cachify.setup(assets, {
+                             root: ['/tmp'],
+                             production: false,
+                             debug: true});
+    links = cachify.cachify_js("/js/main.min.js").split('\n');
     test.equal(links[0], '<script src="/d41d8cd98f00b204e9800998ecf8427e/js/lib/jquery.js"></script>',
               "debug option puts hash in all urls");
     test.done();
   },
   "Development mode": function (test) {
-    var assets = {
-            "/js/main.min.js": [
-            "/js/lib/jquery.js", "/js/lib/jquery-foomatic.js",
-            "/js/main.js"
-          ]
-        },
+    var assets = make_assets(),
         req = {
           url: '/d41d8cd98f00b204e9800998ecf8427e/js/main.min.js'
         },
@@ -112,6 +108,8 @@ exports.setup = nodeunit.testCase({
                       production: false
     });
     var links = cachify.cachify_js("/js/main.min.js").split('\n');
+    console.log('links', links.length);
+    test.ok(links.length, "Multiple script tags");
     test.equal(links[0], '<script src="/js/lib/jquery.js"></script>',
               "No hashes in all urls during development");
     mddlwr(req, resp, function () {
@@ -121,6 +119,37 @@ exports.setup = nodeunit.testCase({
       test.ok(resp.state.header > 0);
       test.done();
     });
-    
+  },
+  "Production mode": function (test) {
+    var assets = make_assets(),
+        req = {
+          url: '/d41d8cd98f00b204e9800998ecf8427e/js/main.min.js'
+        },
+        resp = {
+          state: {},
+          local: function (key, value) {
+            this.state[key] = value;
+          },
+          setHeader: function (key, value) {
+            this.state[key] = value;
+            if (!this.state['header']) this.state['header'] = 0;
+            this.state['header'] += 1;
+          }
+        },
+        mddlwr;
+    mddlwr = cachify.setup(
+        assets, {
+          root: ['/tmp']
+    });
+    var links = cachify.cachify_js("/js/main.min.js");
+    test.equal(links, '<script src="/d41d8cd98f00b204e9800998ecf8427e/js/main.min.js"></script>',
+              "Hashes in all urls in production");
+    mddlwr(req, resp, function () {
+      console.log(resp.state);
+      test.ok(resp.state.cachify_js);
+      test.ok(resp.state.cachify_css);
+      test.ok(resp.state.header > 0);
+      test.done();
+    });
   }
 });
